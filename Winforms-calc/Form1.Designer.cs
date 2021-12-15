00945194782162282bde61a9d6e9a91b13006b3a
michael <michaelbecquer@yahoo.fr>
2021-12-15 14:34:39 +0100

    remake buttons

--- Winforms-calc/Form1.Designer.cs
+++ Winforms-calc/Form1.Designer.cs
@@ -32,78 +32,207 @@ partial class Form1
     {
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 200);
+        this.ClientSize = new System.Drawing.Size(800, 600);
         this.Text = "Form1";
 
-        //labelOriginal
+        // //labelOriginal
+        // lblOriginal = new Label();
+        // lblOriginal.Name = "lblOriginal";
+        // lblOriginal.Text = "Original :";
+        // lblOriginal.Location = new System.Drawing.Point(20, 20);
+        // this.Controls.Add(lblOriginal);
+
+        // //labelCopie
+        // lblCopie = new Label();
+        // lblCopie.Name = "lblCopie";
+        // lblCopie.Text = "Copie :";
+        // lblCopie.Location = new System.Drawing.Point(20, 50);
+        // this.Controls.Add(lblCopie);
+
+        // //labelResultat
+        // lblResultat = new Label();
+        // lblResultat.Name = "lblResultat";
+        // lblResultat.Text = "Resultat";
+        // lblResultat.Location = new System.Drawing.Point(200, 50);
+        // lblResultat.Size = new System.Drawing.Size(200, 50);
+        // this.Controls.Add(lblResultat);
+
+        // //label coloré
+        // lblColor = new Label();
+        // lblColor.Name = "lblColor";
+        // lblColor.Text = "La couleur choisie";
+        // lblColor.BackColor = Color.Red;
+        // lblColor.AutoSize = false;
+        // lblColor.Location = new System.Drawing.Point(20, 70);
+        // lblColor.Size = new System.Drawing.Size(200, 50);
+        // this.Controls.Add(lblColor);
+
+        // //textbox or input
+        // input = new TextBox();
+        // input.Name = "input";
+        // // input.Text;
+        // input.Location = new System.Drawing.Point(200, 20);
+        // input.Size = new System.Drawing.Size(200, 50);
+        // this.Controls.Add(input);
+
+        // //button Recopie
+        // btnRecopie = new Button();
+        // btnRecopie.Name = "btnRecopie";
+        // btnRecopie.Text = "Recopier";
+        // // btn1.ForeColor = Color.FromArgb(255, 76, 41);
+        // // btn1.BackColor = Color.FromArgb(8, 32, 50);
+        // btnRecopie.Location = new System.Drawing.Point(600, 20);
+        // // btn1.Size = new System.Drawing.Size(100, 50);
+        // this.Controls.Add(btnRecopie);
+
+        // //button Delete
+        // btnDelete = new Button();
+        // btnDelete.Name = "btnDelete";
+        // btnDelete.Text = "Effacer";
+        // btnDelete.Location = new System.Drawing.Point(600, 50);
+        // // btnDelete.Size = new System.Drawing.Size(100, 50);
+        // this.Controls.Add(btnDelete);
+
+        // //button Quit
+        // btnQuit = new Button();
+        // btnQuit.Name = "btnQuit";
+        // btnQuit.Text = "Quitter";
+        // btnQuit.Location = new System.Drawing.Point(600, 80);
+        // // btnQuit.Size = new System.Drawing.Size(100, 50);
+        // this.Controls.Add(btnQuit);
+
+
+
+        // //radio button rouge
+        // rbRouge = new RadioButton();
+        // rbRouge.Name = "rbRouge";
+        // rbRouge.Text = "&Rouge";
+        // rbRouge.Checked = true;
+        // this.Controls.Add(rbRouge);
+
+
+
+        // //radio button bleu
+        // rbBleu = new RadioButton();
+        // rbBleu.Name = "rbBleu";
+        // rbBleu.Text = "&Bleu";
+        // rbBleu.Checked = false;
+        // this.Controls.Add(rbBleu);
+
+
+        // //groupbox
+        // gbxCouleur = new GroupBox();
+        // gbxCouleur.Name = "gbxCouleur";
+        // gbxCouleur.Text = "Couleur";
+        // gbxCouleur.Visible = false;
+        // gbxCouleur.Controls.Add(rbVert);
+        // gbxCouleur.Location = new System.Drawing.Point(100, 200);
+        // gbxCouleur.Size = new System.Drawing.Size(300, 100);
+        // this.Controls.Add(gbxCouleur);
+
+
+
+
+        // //checkbox
+        // chkModifier = new CheckBox();
+        // chkModifier.Name = "chkModifier";
+        // chkModifier.Text = "Modifier la couleur";
+        // chkModifier.Location = new System.Drawing.Point(200, 100);
+        // chkModifier.Size = new System.Drawing.Size(100, 50);
+        // this.chkModifier.CheckedChanged += new EventHandler(chkModifier_CheckedChanged);
+        // this.Controls.Add(chkModifier);
+
+        //déclaration des boutons etc
+
+        gbxCouleur = new GroupBox();
+        rbRouge = new RadioButton();
+        rbVert = new RadioButton();
+        rbBleu = new RadioButton();
         lblOriginal = new Label();
-        lblOriginal.Name = "lblOriginal";
-        lblOriginal.Text = "Original :";
-        lblOriginal.Location = new System.Drawing.Point(20, 20);
-        this.Controls.Add(lblOriginal);
-
-        //labelCopie
         lblCopie = new Label();
-        lblCopie.Name = "lblCopie";
-        lblCopie.Text = "Copie :";
-        lblCopie.Location = new System.Drawing.Point(20, 50);
-        this.Controls.Add(lblCopie);
-
-        //labelResultat
         lblResultat = new Label();
+        lblColor = new Label();
+        chkModifier = new CheckBox();
+        input = new TextBox();
+        btnRecopier = new Button();
+        btnEffacer = new Button();
+        btnQuitter = new Button();
+
+        //gbxcouleur
+        gbxCouleur.Controls.Add(rbRouge);
+        gbxCouleur.Controls.Add(rbVert);
+        gbxCouleur.Controls.Add(rbBleu);
+        gbxCouleur.Location = new System.Drawing.Point(50, 400);
+        gbxCouleur.Size = new System.Drawing.Size(200, 100);
+        gbxCouleur.Text = "Radio buttons";
+        //rouge 
+        rbRouge.Text = "&Rouge";
+        rbVert.Checked = true;
+        rbRouge.Location = new System.Drawing.Point(31, 20);
+        rbRouge.Size = new System.Drawing.Size(67, 20);
+        //vert
+        rbVert.Text = "&Vert";
+        rbVert.Checked = false;
+        rbVert.Location = new System.Drawing.Point(31, 40);
+        rbVert.Size = new System.Drawing.Size(67, 20);
+        // this.rbVert.CheckedChanged += new EventHandler(rbVert_CheckedChanged);
+        //bleu
+        rbBleu.Text = "&Bleu";
+        rbVert.Checked = false;
+        rbBleu.Location = new System.Drawing.Point(31, 60);
+        rbBleu.Size = new System.Drawing.Size(67, 20);
+        //lblOriginal
+        lblOriginal.Name = "lblOriginal";
+        lblOriginal.Text = "Original : ";
+        lblOriginal.Location = new System.Drawing.Point(50, 50);
+        //lblCopie
+        lblCopie.Name = "lblCopie";
+        lblCopie.Text = "Copie : ";
+        lblCopie.Location = new System.Drawing.Point(50, 80);
+        //lblResultat
         lblResultat.Name = "lblResultat";
         lblResultat.Text = "Resultat";
-        lblResultat.Location = new System.Drawing.Point(200, 50);
-        lblResultat.Size = new System.Drawing.Size(200, 50);
-        this.Controls.Add(lblResultat);
-
-        //textbox or input
-        input = new TextBox();
+        lblResultat.Location = new System.Drawing.Point(200, 80);
+        //lblColor
+        lblColor.Name = "lblColor";
+        lblColor.Text = "La couleur choisie";
+        lblColor.BackColor = Color.Red;
+        lblColor.AutoSize = false;
+        lblColor.Location = new System.Drawing.Point(300, 400);
+        lblColor.Size = new System.Drawing.Size(200, 100);
+        //input
         input.Name = "input";
-        // input.Text;
-        input.Location = new System.Drawing.Point(200, 20);
-        input.Size = new System.Drawing.Size(200, 50);
-        this.Controls.Add(input);
+        input.Text = "Entrer le texte initial";
+        input.Location = new System.Drawing.Point(200, 50);
+        input.Size = new System.Drawing.Size(150, 20);
+        //button recopier
+        btnRecopier.Name = "btnRecopier";
+        btnRecopier.Text = "Recopier";
+        btnRecopier.Location = new System.Drawing.Point(600, 50);
+        //button effacer
+        btnEffacer.Name = "btnEffacer";
+        btnEffacer.Text = "Effacer";
+        btnEffacer.Location = new System.Drawing.Point(600, 80);
+        //button quitter
+        btnQuitter.Name = "btnQuitter";
+        btnQuitter.Text = "Quitter";
+        btnQuitter.Location = new System.Drawing.Point(600, 110);
+        //checkbox
+        chkModifier.Name = "chkModifier";
+        chkModifier.Text = "Modifier la couleur";
+        chkModifier.Location = new System.Drawing.Point(50, 350);
+        chkModifier.Size = new System.Drawing.Size(200, 20);
 
-        //button Recopie
-        btnRecopie = new Button();
-        btnRecopie.Name = "btnRecopie";
-        btnRecopie.Text = "Recopier";
-        // btn1.ForeColor = Color.FromArgb(255, 76, 41);
-        // btn1.BackColor = Color.FromArgb(8, 32, 50);
-        btnRecopie.Location = new System.Drawing.Point(600, 20);
-        // btn1.Size = new System.Drawing.Size(100, 50);
-        this.Controls.Add(btnRecopie);
+        //add
+        this.Controls.AddRange(new Control[] { gbxCouleur, lblOriginal, lblCopie, lblResultat, lblColor, chkModifier, input, btnRecopier, btnEffacer, btnQuitter });
 
-        //button Delete
-        btnDelete = new Button();
-        btnDelete.Name = "btnDelete";
-        btnDelete.Text = "Effacer";
-        btnDelete.Location = new System.Drawing.Point(600, 50);
-        // btnDelete.Size = new System.Drawing.Size(100, 50);
-        this.Controls.Add(btnDelete);
 
-        //button Quit
-        btnQuit = new Button();
-        btnQuit.Name = "btnQuit";
-        btnQuit.Text = "Quitter";
-        btnQuit.Location = new System.Drawing.Point(600, 80);
-        // btnQuit.Size = new System.Drawing.Size(100, 50);
-        this.Controls.Add(btnQuit);
-
-        //groupbox
-        gb1 = new GroupBox();
-        this.Controls.Add(gb1);
-        //radio button
-        rb1 = new RadioButton();
-        rb1.Text = "radio button : ";
-        rb1.Checked = true;
-        this.Controls.Add(rb1);
     }
-    private Label lblOriginal, lblCopie, lblResultat;
+    private Button btnRecopier, btnEffacer, btnQuitter;
+    private Label lblOriginal, lblCopie, lblResultat, lblColor;
     private TextBox input;
-    private Button btnRecopie, btnDelete, btnQuit;
-    private GroupBox gb1;
-    private RadioButton rb1;
+    private CheckBox chkModifier;
+    private GroupBox gbxCouleur;
+    private RadioButton rbRouge, rbVert, rbBleu;
     #endregion
 }
--- Winforms-calc/Form1.cs
+++ Winforms-calc/Form1.cs
@@ -2,40 +2,55 @@ namespace Winforms_calc;
 
 public partial class Form1 : Form
 {
-    private string leTexte;
+    // private string leTexte;
     public Form1()
     {
         InitializeComponent();
-        btnRecopie.Click += btnRecopier_Click;
-        btnDelete.Click += btnEffacer_Click;
-        btnQuit.Click += btnQuitter_Click;
-        this.input.GotFocus += new EventHandler(txtInput_GotFocus);
-        this.leTexte = "Entrer le texte initial";
-        this.input.Text = leTexte;
+        //  btnRecopie.Click += btnRecopier_Click;
+        //  btnDelete.Click += btnEffacer_Click;
+        //  btnQuit.Click += btnQuitter_Click;
+        //  this.input.GotFocus += new EventHandler(txtInput_GotFocus);
+        //  this.leTexte = "Entrer le texte initial";
+        //this.input.Text = leTexte;
+        //this.Height = 200;
     }
 
-    private void btnRecopier_Click(object sender, EventArgs e)
-    {
-       this.lblResultat.Text = this.input.Text;
-    }
-    private void btnEffacer_Click(object sender, EventArgs e)
-    {
-      this.input.Text = this.leTexte;
-      this.lblResultat.Text = "";
-    }
-    private void btnQuitter_Click(object sender, EventArgs e)
-    {
-        if (MessageBox.Show("Voulez vous vraiment quitter", "Terminer ?", MessageBoxButtons.OKCancel) == DialogResult.OK)
-        {
-            Application.Exit();
-        }
-    }
-    private void Form1_Load(object sender, EventArgs e){
-        this.input.Text = "Entrer le texte initial";
-    }
-    private void txtInput_GotFocus(object sender, EventArgs e){
-        this.input.Text = "";
-    }
+    // private void btnRecopier_Click(object sender, EventArgs e)
+    // {
+    //     this.lblResultat.Text = this.input.Text;
+    // }
+    // private void btnEffacer_Click(object sender, EventArgs e)
+    // {
+    //     this.input.Text = this.leTexte;
+    //     this.lblResultat.Text = "";
+    // }
+    // private void btnQuitter_Click(object sender, EventArgs e)
+    // {
+    //     if (MessageBox.Show("Voulez vous vraiment quitter", "Terminer ?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+    //     {
+    //         Application.Exit();
+    //     }
+    // }
+    // private void Form1_Load(object sender, EventArgs e)
+    // {
+    //     this.input.Text = "Entrer le texte initial";
+    // }
+    // private void txtInput_GotFocus(object sender, EventArgs e)
+    // {
+    //     this.input.Text = "";
+    // }
+
+
+    // private void chkModifier_CheckedChanged(object sender, EventArgs e)
+    // {
+    //     this.gbxCouleur.Visible = this.chkModifier.Checked;
+    //     this.Height = this.chkModifier.Checked ? 400 : 200;
+    // }
+    // private void rbVert_CheckedChanged(object sender, EventArgs e){
+    //     if(this.rbVert.Checked){
+    //         this.lblColor.BackColor = System.Drawing.Color.Green;
+    //     }
+    // }
 
 }
 
