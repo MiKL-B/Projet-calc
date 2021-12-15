e111b99bcf26c0ed9ee1ccf3e8701401f1bd6223
michael <michaelbecquer@yahoo.fr>
2021-12-15 09:38:56 +0100

    feat: add 3 buttons

--- Winforms-calc/Form1.Designer.cs
+++ Winforms-calc/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace Winforms_calc;
+﻿using System.Windows.Forms;
+namespace Winforms_calc;
 
 partial class Form1
 {
@@ -32,7 +33,31 @@ partial class Form1
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
         this.Text = "Form1";
+        //button 1
+        btn1 = new Button();
+        btn1.Name = "btn1";
+        btn1.Text = "Recopier";
+        btn1.Location = new System.Drawing.Point(600, 20);
+        btn1.Size = new System.Drawing.Size(100, 50);
+        this.Controls.Add(btn1);
+        //button 2
+        btnDelete = new Button();
+        btnDelete.Name = "btn1";
+        btnDelete.Text = "Effacer";
+        btnDelete.Location = new System.Drawing.Point(600, 120);
+        btnDelete.Size = new System.Drawing.Size(100, 50);
+        this.Controls.Add(btnDelete);
+        //button 3
+        btnQuit = new Button();
+        btnQuit.Name = "btn1";
+        btnQuit.Text = "Quitter";
+        btnQuit.Location = new System.Drawing.Point(600, 240);
+        btnQuit.Size = new System.Drawing.Size(100, 50);
+        this.Controls.Add(btnQuit);
     }
+    private Button btn1;
+    private Button btnDelete;
+    private Button btnQuit;
 
     #endregion
 }
--- Winforms-calc/Form1.cs
+++ Winforms-calc/Form1.cs
@@ -5,5 +5,21 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        btn1.Click += btnRecopier_Click;
+        btnDelete.Click += btnEffacer_Click;
+        btnQuit.Click += btnQuitter_Click;
+    }
+
+    private void btnRecopier_Click(object sender, EventArgs e)
+    {
+        btn1.Text = this.Text;
+    }
+    private void btnEffacer_Click(object sender, EventArgs e)
+    {
+        btnDelete.Text = "";
+    }
+    private void btnQuitter_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
     }
 }
