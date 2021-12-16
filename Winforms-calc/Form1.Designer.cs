b024cec017b7d6c5ee0e025d1d12554513a759d8
michael <michaelbecquer@yahoo.fr>
2021-12-16 16:02:40 +0100

    refonte calc

--- Winforms-calc/Form1.Designer.cs
+++ Winforms-calc/Form1.Designer.cs
@@ -28,238 +28,155 @@ partial class Form1
     ///  Required method for Designer support - do not modify
     ///  the contents of this method with the code editor.
     /// </summary>
+
+    public class ButtonCalc : Button
+    {
+
+
+        public ButtonCalc(string name, string text, int xPos, int yPos, int xSiz, int ySiz)
+        {
+            this.Name = Name;
+            this.Text = text;
+            this.Location = new System.Drawing.Point(xPos, yPos);
+            this.Size = new System.Drawing.Size(xSiz, ySiz);
+        }
+    }
+    private GroupBox gbAffichage, gbNumber;
+    // private ComboBox comboVariable;
+    // private ComboBox comboFunction;
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(400, 600);
         this.Text = "Form1";
+
+
+        ButtonCalc Modulo = new ButtonCalc("Unary", "%", 0, 80, 100, 50);
+        ButtonCalc CE = new ButtonCalc("CE", "CE", 100, 80, 100, 50);
+        ButtonCalc C = new ButtonCalc("C", "CE", 200, 80, 100, 50);
+        ButtonCalc Return = new ButtonCalc("Return", "Return", 300, 80, 100, 50);
+        ButtonCalc FunctionInverse = new ButtonCalc("FunctionInverse", "1/x", 0, 130, 100, 50);
+        ButtonCalc Square = new ButtonCalc("Square", "x²", 100, 130, 100, 50);
+        ButtonCalc Sqrt = new ButtonCalc("Sqrt", "²√x", 200, 130, 100, 50);
+        ButtonCalc Division = new ButtonCalc("Division", "/", 300, 130, 100, 50);
+        ButtonCalc Seven = new ButtonCalc("Seven", "7", 0, 180, 100, 50);
+        ButtonCalc Eight = new ButtonCalc("Eight", "8", 100, 180, 100, 50);
+        ButtonCalc Nine = new ButtonCalc("Nine", "9", 200, 180, 100, 50);
+        ButtonCalc Multiply = new ButtonCalc("Multiply", "*", 300, 180, 100, 50);
+        ButtonCalc Four = new ButtonCalc("Four", "4", 0, 230, 100, 50);
+        ButtonCalc Five = new ButtonCalc("Five", "5", 100, 230, 100, 50);
+        ButtonCalc Six = new ButtonCalc("Six", "6", 200, 230, 100, 50);
+        ButtonCalc Minus = new ButtonCalc("Minus", "-", 300, 230, 100, 50);
+        ButtonCalc One = new ButtonCalc("One", "1", 0, 280, 100, 50);
+        ButtonCalc Two = new ButtonCalc("Two", "2", 100, 280, 100, 50);
+        ButtonCalc Three = new ButtonCalc("Three", "3", 200, 280, 100, 50);
+        ButtonCalc Plus = new ButtonCalc("Plus", "+", 300, 280, 100, 50);
+        ButtonCalc Unary = new ButtonCalc("Unary", "+/-", 0, 330, 100, 50);
+        ButtonCalc Zero = new ButtonCalc("Zero", "0", 100, 330, 100, 50);
+        ButtonCalc Comma = new ButtonCalc("Comma", ",", 200, 330, 100, 50);
+        ButtonCalc Equal = new ButtonCalc("Equal", "=", 300, 330, 100, 50);
+
+
+        Button[] mesButtons = new Button[24];
+        mesButtons[0] = Modulo;
+        mesButtons[1] = CE;
+        mesButtons[2] = C;
+        mesButtons[3] = Return;
+        mesButtons[4] = FunctionInverse;
+        mesButtons[5] = Square;
+        mesButtons[6] = Sqrt;
+        mesButtons[7] = Division;
+        mesButtons[8] = Seven;
+        mesButtons[9] = Eight;
+        mesButtons[10] = Nine;
+        mesButtons[11] = Multiply;
+        mesButtons[12] = Four;
+        mesButtons[13] = Five;
+        mesButtons[14] = Six;
+        mesButtons[15] = Minus;
+        mesButtons[16] = One;
+        mesButtons[17] = Two;
+        mesButtons[18] = Three;
+        mesButtons[19] = Plus;
+        mesButtons[20] = Unary;
+        mesButtons[21] = Zero;
+        mesButtons[22] = Comma;
+        mesButtons[23] = Equal;
+
+
         //
         //déclaration
         //
         gbAffichage = new GroupBox();
         gbNumber = new GroupBox();
-        comboVariable = new ComboBox();
-        comboFunction = new ComboBox();
-        buttonUnary = new Button();
-        buttonZero = new Button();
-        buttonComma = new Button();
-        buttonEqual = new Button();
-        buttonOne = new Button();
-        buttonTwo = new Button();
-        buttonThree = new Button();
-        buttonPlus = new Button();
-        buttonFour = new Button();
-        buttonFive = new Button();
-        buttonSix = new Button();
-        buttonMinus = new Button();
-        buttonSeven = new Button();
-        buttonEight = new Button();
-        buttonNine = new Button();
-        buttonMultiply = new Button();
-        buttonFunctionInverse = new Button();
-        buttonSquare = new Button();
-        buttonSqrt = new Button();
-        buttonDivision = new Button();
-        buttonModulo = new Button();
-        buttonCE = new Button();
-        buttonC = new Button();
-        buttonReturn = new Button();
+        foreach (var item in mesButtons)
+        {
+            gbNumber.Controls.Add(item);
+        }
+        gbNumber.Location = new System.Drawing.Point(0, 220);
+        gbNumber.Size = new System.Drawing.Size(400, 380);
+        // gbNumber.BackColor = Color.FromArgb(32, 32, 32);
+
+        // comboVariable = new ComboBox();
+        // comboFunction = new ComboBox();
+        // buttonLisp = new Button();
+        // buttonRPN = new Button();
+        // buttonMemory = new Button();
+        // buttonAffect = new Button();
         //
         //GROUP BOX//
         //
 
         //group box affichage
-        gbAffichage.Controls.AddRange(new Control[] { });
-        gbAffichage.Text = "Affichage";
-        gbAffichage.Location = new System.Drawing.Point(0, 20);
-        gbAffichage.Size = new System.Drawing.Size(400, 200);
-        gbAffichage.BackColor = Color.Gray;
+        // gbAffichage.Controls.AddRange(new Control[] { });
+        // gbAffichage.Text = "Affichage";
+        // gbAffichage.Location = new System.Drawing.Point(0, 20);
+        // gbAffichage.Size = new System.Drawing.Size(400, 200);
+        // gbAffichage.BackColor = Color.FromArgb(32, 32, 32);
+        // gbAffichage.ForeColor = Color.LightGray;
 
-        //group box number
-        gbNumber.Controls.Add(buttonUnary);
-        gbNumber.Controls.Add(buttonZero);
-        gbNumber.Controls.Add(buttonComma);
-        gbNumber.Controls.Add(buttonEqual);
-        gbNumber.Controls.Add(buttonOne);
-        gbNumber.Controls.Add(buttonTwo);
-        gbNumber.Controls.Add(buttonThree);
-        gbNumber.Controls.Add(buttonPlus);
-        gbNumber.Controls.Add(buttonFour);
-        gbNumber.Controls.Add(buttonFive);
-        gbNumber.Controls.Add(buttonSix);
-        gbNumber.Controls.Add(buttonMinus);
-        gbNumber.Controls.Add(buttonSeven);
-        gbNumber.Controls.Add(buttonEight);
-        gbNumber.Controls.Add(buttonNine);
-        gbNumber.Controls.Add(buttonMultiply);
-        gbNumber.Controls.Add(buttonFunctionInverse);
-        gbNumber.Controls.Add(buttonSquare);
-        gbNumber.Controls.Add(buttonSqrt);
-        gbNumber.Controls.Add(buttonDivision);
-        gbNumber.Controls.Add(buttonModulo);
-        gbNumber.Controls.Add(buttonCE);
-        gbNumber.Controls.Add(buttonC);
-        gbNumber.Controls.Add(buttonReturn);
-        gbNumber.Controls.Add(comboVariable);
-        gbNumber.Controls.Add(comboFunction);
-        gbNumber.Location = new System.Drawing.Point(0, 220);
-        gbNumber.Size = new System.Drawing.Size(400, 380);
-        gbNumber.BackColor = Color.Gray;
+
         //
         //COMBOBOX
         //
-        comboVariable.Text = "Variables";
-        comboVariable.Location = new System.Drawing.Point(0, 10);
-        comboFunction.Text = "Fonctions";
-        comboFunction.Location = new System.Drawing.Point(0, 40);
-        //
-        //BUTTONS
-        //
+        // comboVariable.Text = "Variables";
+        // comboVariable.Location = new System.Drawing.Point(0, 10);
+        // comboVariable.Size = new System.Drawing.Size(100, 50);
+        // comboFunction.Text = "Fonctions";
+        // comboFunction.Location = new System.Drawing.Point(0, 40);
+        // comboFunction.Size = new System.Drawing.Size(100, 50);
+        // //
+        // //BUTTONS
+        // //
 
-        //button unary
-        buttonUnary.Text = "+/-";
-        buttonUnary.Location = new System.Drawing.Point(0, 330);
-        buttonUnary.Size = new System.Drawing.Size(100, 50);
-        buttonUnary.BackColor = Color.White;
 
-        //button zero
-        buttonZero.Text = "0";
-        buttonZero.Location = new System.Drawing.Point(100, 330);
-        buttonZero.Size = new System.Drawing.Size(100, 50);
-        buttonZero.BackColor = Color.White;
 
-        //button comma
-        buttonComma.Text = ",";
-        buttonComma.Location = new System.Drawing.Point(200, 330);
-        buttonComma.Size = new System.Drawing.Size(100, 50);
-        buttonComma.BackColor = Color.White;
 
-        //button equal
-        buttonEqual.Text = "=";
-        buttonEqual.Location = new System.Drawing.Point(300, 330);
-        buttonEqual.Size = new System.Drawing.Size(100, 50);
-        buttonEqual.BackColor = Color.SkyBlue;
 
-        //button one
-        buttonOne.Text = "1";
-        buttonOne.Location = new System.Drawing.Point(0, 280);
-        buttonOne.Size = new System.Drawing.Size(100, 50);
-        buttonOne.BackColor = Color.White;
+        // //button lisp
+        // buttonLisp.Text = "LISP";
+        // buttonLisp.Location = new System.Drawing.Point(130, 10);
+        // buttonLisp.Size = new System.Drawing.Size(50, 25);
+        // buttonLisp.BackColor = Color.LightGray;
 
-        //button two
-        buttonTwo.Text = "2";
-        buttonTwo.Location = new System.Drawing.Point(100, 280);
-        buttonTwo.Size = new System.Drawing.Size(100, 50);
-        buttonTwo.BackColor = Color.White;
+        // //button RPN
+        // buttonRPN.Text = "RPN";
+        // buttonRPN.Location = new System.Drawing.Point(180, 10);
+        // buttonRPN.Size = new System.Drawing.Size(50, 25);
+        // buttonRPN.BackColor = Color.LightGray;
 
-        //button three
-        buttonThree.Text = "3";
-        buttonThree.Location = new System.Drawing.Point(200, 280);
-        buttonThree.Size = new System.Drawing.Size(100, 50);
-        buttonThree.BackColor = Color.White;
+        // //button affect
+        // buttonAffect.Text = "←";
+        // buttonAffect.Location = new System.Drawing.Point(230, 10);
+        // buttonAffect.Size = new System.Drawing.Size(50, 25);
+        // buttonAffect.BackColor = Color.LightGray;
 
-        //button plus
-        buttonPlus.Text = "+";
-        buttonPlus.Location = new System.Drawing.Point(300, 280);
-        buttonPlus.Size = new System.Drawing.Size(100, 50);
-        buttonPlus.BackColor = Color.LightGray;
-
-        //button four
-        buttonFour.Text = "4";
-        buttonFour.Location = new System.Drawing.Point(0, 230);
-        buttonFour.Size = new System.Drawing.Size(100, 50);
-        buttonFour.BackColor = Color.White;
-
-        //button five
-        buttonFive.Text = "5";
-        buttonFive.Location = new System.Drawing.Point(100, 230);
-        buttonFive.Size = new System.Drawing.Size(100, 50);
-        buttonFive.BackColor = Color.White;
-
-        //button six
-        buttonSix.Text = "6";
-        buttonSix.Location = new System.Drawing.Point(200, 230);
-        buttonSix.Size = new System.Drawing.Size(100, 50);
-        buttonSix.BackColor = Color.White;
-
-        //button minus
-        buttonMinus.Text = "-";
-        buttonMinus.Location = new System.Drawing.Point(300, 230);
-        buttonMinus.Size = new System.Drawing.Size(100, 50);
-        buttonMinus.BackColor = Color.LightGray;
-
-        //button seven
-        buttonSeven.Text = "7";
-        buttonSeven.Location = new System.Drawing.Point(0, 180);
-        buttonSeven.Size = new System.Drawing.Size(100, 50);
-        buttonSeven.BackColor = Color.White;
-
-        //button eight
-        buttonEight.Text = "8";
-        buttonEight.Location = new System.Drawing.Point(100, 180);
-        buttonEight.Size = new System.Drawing.Size(100, 50);
-        buttonEight.BackColor = Color.White;
-
-        //button nine
-        buttonNine.Text = "9";
-        buttonNine.Location = new System.Drawing.Point(200, 180);
-        buttonNine.Size = new System.Drawing.Size(100, 50);
-        buttonNine.BackColor = Color.White;
-
-        //button multiply
-        buttonMultiply.Text = "*";
-        buttonMultiply.Location = new System.Drawing.Point(300, 180);
-        buttonMultiply.Size = new System.Drawing.Size(100, 50);
-        buttonMultiply.BackColor = Color.LightGray;
-
-        //button function inverse
-        buttonFunctionInverse.Text = "1/x";
-        buttonFunctionInverse.Location = new System.Drawing.Point(0, 130);
-        buttonFunctionInverse.Size = new System.Drawing.Size(100, 50);
-        buttonFunctionInverse.BackColor = Color.LightGray;
-
-        //button carré
-        buttonSquare.Text = "x²";
-        buttonSquare.Location = new System.Drawing.Point(100, 130);
-        buttonSquare.Size = new System.Drawing.Size(100, 50);
-        buttonSquare.BackColor = Color.LightGray;
-
-        //button sqrt
-        buttonSqrt.Text = "²√x";
-        buttonSqrt.Location = new System.Drawing.Point(200, 130);
-        buttonSqrt.Size = new System.Drawing.Size(100, 50);
-        buttonSqrt.BackColor = Color.LightGray;
-
-        //button division
-        buttonDivision.Text = "/";
-        buttonDivision.Location = new System.Drawing.Point(300, 130);
-        buttonDivision.Size = new System.Drawing.Size(100, 50);
-        buttonDivision.BackColor = Color.LightGray;
-
-        //button modulo
-        buttonModulo.Text = "%";
-        buttonModulo.Location = new System.Drawing.Point(0, 80);
-        buttonModulo.Size = new System.Drawing.Size(100, 50);
-        buttonModulo.BackColor = Color.LightGray;
-
-        //button CE
-        buttonCE.Text = "CE";
-        buttonCE.Location = new System.Drawing.Point(100, 80);
-        buttonCE.Size = new System.Drawing.Size(100, 50);
-        buttonCE.BackColor = Color.LightGray;
-
-        //button C
-        buttonC.Text = "C";
-        buttonC.Location = new System.Drawing.Point(200, 80);
-        buttonC.Size = new System.Drawing.Size(100, 50);
-        buttonC.BackColor = Color.LightGray;
-
-        //button return
-        buttonReturn.Text = "Return";
-        buttonReturn.Location = new System.Drawing.Point(300, 80);
-        buttonReturn.Size = new System.Drawing.Size(100, 50);
-        buttonReturn.BackColor = Color.LightGray;
+        // //button memory
+        // buttonMemory.Text = "Memory";
+        // buttonMemory.Location = new System.Drawing.Point(280, 10);
+        // buttonMemory.Size = new System.Drawing.Size(75, 25);
+        // buttonMemory.BackColor = Color.LightGray;
 
 
 
@@ -268,32 +185,8 @@ partial class Form1
         this.Controls.Add(gbNumber);
 
     }
-    private GroupBox gbAffichage, gbNumber;
-    private ComboBox comboVariable;
-    private ComboBox comboFunction;
-    private Button buttonUnary;
-    private Button buttonZero;
-    private Button buttonComma;
-    private Button buttonEqual;
-    private Button buttonOne;
-    private Button buttonTwo;
-    private Button buttonThree;
-    private Button buttonPlus;
-    private Button buttonFour;
-    private Button buttonFive;
-    private Button buttonSix;
-    private Button buttonMinus;
-    private Button buttonSeven;
-    private Button buttonEight;
-    private Button buttonNine;
-    private Button buttonMultiply;
-    private Button buttonFunctionInverse;
-    private Button buttonSquare;
-    private Button buttonSqrt;
-    private Button buttonDivision;
-    private Button buttonModulo;
-    private Button buttonCE;
-    private Button buttonC;
-    private Button buttonReturn;
+
+
+
     #endregion
 }
--- Winforms-calc/Form1.cs
+++ Winforms-calc/Form1.cs
@@ -6,39 +6,39 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-        this.Init();
+        // this.Init();
     }
 
-    private void Init()
-    {
+    // private void Init()
+    // {
 
-        this.comboVariable.Items.Clear();
+    //     this.comboVariable.Items.Clear();
 
-        char[] az = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
-        foreach (var c in az)
-        {
-            this.comboVariable.Items.Add(c);
-        }
+    //     char[] az = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
+    //     foreach (var c in az)
+    //     {
+    //         this.comboVariable.Items.Add(c);
+    //     }
 
 
-        string[] functions = new string[11];
-        functions[0] = "sqrt";
-        functions[1] = "pow";
-        functions[2] = "sin";
-        functions[3] = "cos";
-        functions[4] = "tan";
-        functions[5] = "asin";
-        functions[6] = "acos";
-        functions[7] = "atan";
-        functions[8] = "pi";
-        functions[9] = "e";
-        functions[10] = "ln";
-        foreach (var f in functions)
-        {
-           this.comboFunction.Items.Add(f);
-        }
+    //     string[] functions = new string[11];
+    //     functions[0] = "sqrt";
+    //     functions[1] = "pow";
+    //     functions[2] = "sin";
+    //     functions[3] = "cos";
+    //     functions[4] = "tan";
+    //     functions[5] = "asin";
+    //     functions[6] = "acos";
+    //     functions[7] = "atan";
+    //     functions[8] = "pi";
+    //     functions[9] = "e";
+    //     functions[10] = "ln";
+    //     foreach (var f in functions)
+    //     {
+    //        this.comboFunction.Items.Add(f);
+    //     }
 
-    }
+    // }
 
 }
 
