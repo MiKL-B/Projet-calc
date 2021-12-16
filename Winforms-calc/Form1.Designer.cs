019c8d39a803867be27a9070708c74450ade41ed
michael <michaelbecquer@yahoo.fr>
2021-12-16 10:36:13 +0100

    finish exo 3

--- Winforms-calc/Form1.Designer.cs
+++ Winforms-calc/Form1.Designer.cs
@@ -32,42 +32,42 @@ partial class Form1
     {
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(1000, 800);
+        this.ClientSize = new System.Drawing.Size(1000, 1000);
         this.Text = "Form1";
 
         //déclaration
-        // gbxCouleur = new GroupBox();
+        gbxCouleur = new GroupBox();
         gbComboBox = new GroupBox();
-        // gbText = new GroupBox();
-        // gbPhrase = new GroupBox();
+        gbText = new GroupBox();
+        gbPhrase = new GroupBox();
         listCible = new ListBox();
         cbxSource = new ComboBox();
-        // rbRouge = new RadioButton();
-        // rbVert = new RadioButton();
-        // rbBleu = new RadioButton();
-        // rbPhrase1 = new RadioButton();
-        // rbPhrase2 = new RadioButton();
-        // rbPhrase3 = new RadioButton();
-        // lblOriginal = new Label();
-        // lblCopie = new Label();
-        // lblResultat = new Label();
-        // lblColor = new Label();
-        // lblPhrase1 = new Label();
-        // lblPhrase2 = new Label();
-        // lblPhrase3 = new Label();
-        // lblSearch = new Label();
-        // lblOccurence = new Label();
-        // chkModifier = new CheckBox();
-        // input = new TextBox();
-        // inputPhrase1 = new TextBox();
-        // inputPhrase2 = new TextBox();
-        // inputPhrase3 = new TextBox();
-        // inputSearch = new TextBox();
-        // inputOccurence = new TextBox();
-        // btnRecopier = new Button();
-        // btnEffacer = new Button();
-        // btnQuitter = new Button();
-        // btnSearch = new Button();
+        rbRouge = new RadioButton();
+        rbVert = new RadioButton();
+        rbBleu = new RadioButton();
+        rbPhrase1 = new RadioButton();
+        rbPhrase2 = new RadioButton();
+        rbPhrase3 = new RadioButton();
+        lblOriginal = new Label();
+        lblCopie = new Label();
+        lblResultat = new Label();
+        lblColor = new Label();
+        lblPhrase1 = new Label();
+        lblPhrase2 = new Label();
+        lblPhrase3 = new Label();
+        lblSearch = new Label();
+        lblOccurence = new Label();
+        chkModifier = new CheckBox();
+        input = new TextBox();
+        inputPhrase1 = new TextBox();
+        inputPhrase2 = new TextBox();
+        inputPhrase3 = new TextBox();
+        inputSearch = new TextBox();
+        inputOccurence = new TextBox();
+        btnRecopier = new Button();
+        btnEffacer = new Button();
+        btnQuitter = new Button();
+        btnSearch = new Button();
         btnAjoute = new Button();
         btnAjouteTout = new Button();
         btnSupprime = new Button();
@@ -79,7 +79,7 @@ partial class Form1
         gbComboBox.Controls.AddRange(new Control[] { listCible, cbxSource, btnAjoute, btnAjouteTout, btnSupprime, btnSupprimeTout, btnHaut, btnBas });
         gbComboBox.Text = "Les listes";
         gbComboBox.Visible = true;
-        gbComboBox.Location = new System.Drawing.Point(50, 50);
+        gbComboBox.Location = new System.Drawing.Point(50, 650);
         gbComboBox.Size = new System.Drawing.Size(600, 300);
 
         cbxSource.Location = new System.Drawing.Point(50, 50);
@@ -102,184 +102,184 @@ partial class Form1
         btnBas.Text = "↓";
         btnBas.Location = new System.Drawing.Point(400, 200);
 
-        // //gbxcouleur
-        // gbxCouleur.Controls.AddRange(new Control[] { rbRouge, rbVert, rbBleu ,lblColor});
-        // gbxCouleur.Text = "Radio buttons";
-        // gbxCouleur.Visible = false;
-        // gbxCouleur.Location = new System.Drawing.Point(50, 250);
-        // gbxCouleur.Size = new System.Drawing.Size(600, 200);
+        //gbxcouleur
+        gbxCouleur.Controls.AddRange(new Control[] { rbRouge, rbVert, rbBleu ,lblColor});
+        gbxCouleur.Text = "Radio buttons";
+        gbxCouleur.Visible = false;
+        gbxCouleur.Location = new System.Drawing.Point(50, 250);
+        gbxCouleur.Size = new System.Drawing.Size(600, 200);
 
-        // //rouge 
-        // rbRouge.Text = "&Rouge";
-        // rbRouge.Checked = true;
-        // rbRouge.Location = new System.Drawing.Point(31, 20);
-        // rbRouge.Size = new System.Drawing.Size(67, 20);
-        // this.rbRouge.CheckedChanged += new EventHandler(rbColor_CheckedChanged);
+        //rouge 
+        rbRouge.Text = "&Rouge";
+        rbRouge.Checked = true;
+        rbRouge.Location = new System.Drawing.Point(31, 20);
+        rbRouge.Size = new System.Drawing.Size(67, 20);
+        this.rbRouge.CheckedChanged += new EventHandler(rbColor_CheckedChanged);
 
-        // //vert
-        // rbVert.Text = "&Vert";
-        // rbVert.Checked = false;
-        // rbVert.Location = new System.Drawing.Point(31, 40);
-        // rbVert.Size = new System.Drawing.Size(67, 20);
-        // this.rbVert.CheckedChanged += new EventHandler(rbColor_CheckedChanged);
+        //vert
+        rbVert.Text = "&Vert";
+        rbVert.Checked = false;
+        rbVert.Location = new System.Drawing.Point(31, 40);
+        rbVert.Size = new System.Drawing.Size(67, 20);
+        this.rbVert.CheckedChanged += new EventHandler(rbColor_CheckedChanged);
 
-        // //bleu
-        // rbBleu.Text = "&Bleu";
-        // rbBleu.Checked = false;
-        // rbBleu.Location = new System.Drawing.Point(31, 60);
-        // rbBleu.Size = new System.Drawing.Size(67, 20);
-        // this.rbBleu.CheckedChanged += new EventHandler(rbColor_CheckedChanged);
+        //bleu
+        rbBleu.Text = "&Bleu";
+        rbBleu.Checked = false;
+        rbBleu.Location = new System.Drawing.Point(31, 60);
+        rbBleu.Size = new System.Drawing.Size(67, 20);
+        this.rbBleu.CheckedChanged += new EventHandler(rbColor_CheckedChanged);
 
-        // //gbText
-        // gbText.Controls.AddRange(new Control[]{lblOriginal, lblCopie, lblResultat, input, btnRecopier, btnEffacer, btnQuitter, chkModifier});
-        // gbText.Visible = true;
-        // gbText.Location = new System.Drawing.Point(50,50);
-        // gbText.Size = new System.Drawing.Size(600,200);
+        //gbText
+        gbText.Controls.AddRange(new Control[]{lblOriginal, lblCopie, lblResultat, input, btnRecopier, btnEffacer, btnQuitter, chkModifier});
+        gbText.Visible = true;
+        gbText.Location = new System.Drawing.Point(50,50);
+        gbText.Size = new System.Drawing.Size(600,200);
 
-        // //gbPhrase
-        // gbPhrase.Controls.AddRange(new Control[] { rbPhrase1, rbPhrase2, rbPhrase3 ,lblPhrase1,lblPhrase2,lblPhrase3,inputPhrase1,inputPhrase2,inputPhrase3,inputSearch,inputOccurence,lblSearch,lblOccurence,btnSearch});
-        // gbPhrase.Text = "Rechercher dans";
-        // gbPhrase.Visible = true;
-        // gbPhrase.Location = new System.Drawing.Point(50, 450);
-        // gbPhrase.Size =new System.Drawing.Size(600,200);
+        //gbPhrase
+        gbPhrase.Controls.AddRange(new Control[] { rbPhrase1, rbPhrase2, rbPhrase3 ,lblPhrase1,lblPhrase2,lblPhrase3,inputPhrase1,inputPhrase2,inputPhrase3,inputSearch,inputOccurence,lblSearch,lblOccurence,btnSearch});
+        gbPhrase.Text = "Rechercher dans";
+        gbPhrase.Visible = true;
+        gbPhrase.Location = new System.Drawing.Point(50, 450);
+        gbPhrase.Size =new System.Drawing.Size(600,200);
 
-        // //rbPhrase1 
-        // rbPhrase1.Text = "Phrase 1";
-        // rbPhrase1.Checked = true;
-        // rbPhrase1.Location = new System.Drawing.Point(30, 100);
-        // rbPhrase1.Size = new System.Drawing.Size(100, 20);
+        //rbPhrase1 
+        rbPhrase1.Text = "Phrase 1";
+        rbPhrase1.Checked = true;
+        rbPhrase1.Location = new System.Drawing.Point(30, 100);
+        rbPhrase1.Size = new System.Drawing.Size(100, 20);
 
-        // //rbPhrase2
-        // rbPhrase2.Text = "Phrase 2";
-        // rbPhrase2.Checked = false;
-        // rbPhrase2.Location = new System.Drawing.Point(30, 130);
-        // rbPhrase2.Size = new System.Drawing.Size(100, 20);
+        //rbPhrase2
+        rbPhrase2.Text = "Phrase 2";
+        rbPhrase2.Checked = false;
+        rbPhrase2.Location = new System.Drawing.Point(30, 130);
+        rbPhrase2.Size = new System.Drawing.Size(100, 20);
 
-        // //rbPhrase3
-        // rbPhrase3.Text = "Phrase 3";
-        // rbPhrase3.Checked = false;
-        // rbPhrase3.Location = new System.Drawing.Point(30, 160);
-        // rbPhrase3.Size = new System.Drawing.Size(100, 20);
+        //rbPhrase3
+        rbPhrase3.Text = "Phrase 3";
+        rbPhrase3.Checked = false;
+        rbPhrase3.Location = new System.Drawing.Point(30, 160);
+        rbPhrase3.Size = new System.Drawing.Size(100, 20);
 
-        // //lblOriginal
-        // lblOriginal.Name = "lblOriginal";
-        // lblOriginal.Text = "Original : ";
-        // lblOriginal.Location = new System.Drawing.Point(50, 50);
+        //lblOriginal
+        lblOriginal.Name = "lblOriginal";
+        lblOriginal.Text = "Original : ";
+        lblOriginal.Location = new System.Drawing.Point(50, 50);
 
-        // //lblCopie
-        // lblCopie.Name = "lblCopie";
-        // lblCopie.Text = "Copie : ";
-        // lblCopie.Location = new System.Drawing.Point(50, 80);
+        //lblCopie
+        lblCopie.Name = "lblCopie";
+        lblCopie.Text = "Copie : ";
+        lblCopie.Location = new System.Drawing.Point(50, 80);
 
-        // //lblResultat
-        // lblResultat.Name = "lblResultat";
-        // lblResultat.Text = "Resultat";
-        // lblResultat.Location = new System.Drawing.Point(200, 80);
+        //lblResultat
+        lblResultat.Name = "lblResultat";
+        lblResultat.Text = "Resultat";
+        lblResultat.Location = new System.Drawing.Point(200, 80);
 
-        // //lblColor
-        // lblColor.Name = "lblColor";
-        // lblColor.Text = "La couleur choisie";
-        // lblColor.BackColor = Color.Red;
-        // lblColor.AutoSize = false;
-        // lblColor.Location = new System.Drawing.Point(300, 50);
-        // lblColor.Size = new System.Drawing.Size(100, 100);
+        //lblColor
+        lblColor.Name = "lblColor";
+        lblColor.Text = "La couleur choisie";
+        lblColor.BackColor = Color.Red;
+        lblColor.AutoSize = false;
+        lblColor.Location = new System.Drawing.Point(300, 50);
+        lblColor.Size = new System.Drawing.Size(100, 100);
 
-        // //lblPhrase1
-        // lblPhrase1.Name = "lblPhrase1";
-        // lblPhrase1.Text = "Phrase 1 ";
-        // lblPhrase1.Location = new System.Drawing.Point(30, 20);
-        // lblPhrase1.Size = new System.Drawing.Size(67, 20);
+        //lblPhrase1
+        lblPhrase1.Name = "lblPhrase1";
+        lblPhrase1.Text = "Phrase 1 ";
+        lblPhrase1.Location = new System.Drawing.Point(30, 20);
+        lblPhrase1.Size = new System.Drawing.Size(67, 20);
 
-        // //lblPhrase2
-        // lblPhrase2.Name = "lblPhrase2";
-        // lblPhrase2.Text = "Phrase 2 ";
-        // lblPhrase2.Location = new System.Drawing.Point(30, 40);
-        // lblPhrase2.Size = new System.Drawing.Size(67, 20);
+        //lblPhrase2
+        lblPhrase2.Name = "lblPhrase2";
+        lblPhrase2.Text = "Phrase 2 ";
+        lblPhrase2.Location = new System.Drawing.Point(30, 40);
+        lblPhrase2.Size = new System.Drawing.Size(67, 20);
 
-        // //lblPhrase3
-        // lblPhrase3.Name = "lblPhrase3";
-        // lblPhrase3.Text = "Phrase 3 ";
-        // lblPhrase3.Location = new System.Drawing.Point(30, 60);
-        // lblPhrase3.Size = new System.Drawing.Size(67, 20);
+        //lblPhrase3
+        lblPhrase3.Name = "lblPhrase3";
+        lblPhrase3.Text = "Phrase 3 ";
+        lblPhrase3.Location = new System.Drawing.Point(30, 60);
+        lblPhrase3.Size = new System.Drawing.Size(67, 20);
 
-        // //lblSearch
-        // lblSearch.Name = "lblSearch";
-        // lblSearch.Text = "Lettre à rechercher";
-        // lblSearch.Location = new System.Drawing.Point(150, 100);
-        // lblSearch.Size = new System.Drawing.Size(150, 20);
+        //lblSearch
+        lblSearch.Name = "lblSearch";
+        lblSearch.Text = "Lettre à rechercher";
+        lblSearch.Location = new System.Drawing.Point(150, 100);
+        lblSearch.Size = new System.Drawing.Size(150, 20);
 
-        // //lblOccurence
-        // lblOccurence.Name = "lblOccurence";
-        // lblOccurence.Text = "Nombre d'occurences";
-        // lblOccurence.Location = new System.Drawing.Point(150, 125);
-        // lblOccurence.Size = new System.Drawing.Size(150, 20);
+        //lblOccurence
+        lblOccurence.Name = "lblOccurence";
+        lblOccurence.Text = "Nombre d'occurences";
+        lblOccurence.Location = new System.Drawing.Point(150, 125);
+        lblOccurence.Size = new System.Drawing.Size(150, 20);
 
-        // //input
-        // input.Name = "input";
-        // input.Text = "Entrer le texte initial";
-        // input.Location = new System.Drawing.Point(200, 50);
-        // input.Size = new System.Drawing.Size(150, 20);
+        //input
+        input.Name = "input";
+        input.Text = "Entrer le texte initial";
+        input.Location = new System.Drawing.Point(200, 50);
+        input.Size = new System.Drawing.Size(150, 20);
 
-        // //inputPhrase1
-        // inputPhrase1.Name = "inputPhrase1";
-        // inputPhrase1.Text = "";
-        // inputPhrase1.Location = new System.Drawing.Point(150, 25);
-        // inputPhrase1.Size = new System.Drawing.Size(200, 20);
+        //inputPhrase1
+        inputPhrase1.Name = "inputPhrase1";
+        inputPhrase1.Text = "";
+        inputPhrase1.Location = new System.Drawing.Point(150, 25);
+        inputPhrase1.Size = new System.Drawing.Size(200, 20);
 
-        // //inputPhrase2
-        // inputPhrase2.Name = "inputPhrase2";
-        // inputPhrase2.Text = "";
-        // inputPhrase2.Location = new System.Drawing.Point(150, 50);
-        // inputPhrase2.Size = new System.Drawing.Size(200, 20);
+        //inputPhrase2
+        inputPhrase2.Name = "inputPhrase2";
+        inputPhrase2.Text = "";
+        inputPhrase2.Location = new System.Drawing.Point(150, 50);
+        inputPhrase2.Size = new System.Drawing.Size(200, 20);
 
-        // //inputPhrase3
-        // inputPhrase3.Name = "inputPhrase3";
-        // inputPhrase3.Text = "";
-        // inputPhrase3.Location = new System.Drawing.Point(150, 75);
-        // inputPhrase3.Size = new System.Drawing.Size(200, 20);
+        //inputPhrase3
+        inputPhrase3.Name = "inputPhrase3";
+        inputPhrase3.Text = "";
+        inputPhrase3.Location = new System.Drawing.Point(150, 75);
+        inputPhrase3.Size = new System.Drawing.Size(200, 20);
 
-        // //inputSearch
-        // inputSearch.Name = "inputSearch";
-        // inputSearch.Text = "";
-        // inputSearch.Location = new System.Drawing.Point(300, 100);
-        // inputSearch.Size = new System.Drawing.Size(100, 20);
-        // //inputOccurence
-        // inputOccurence.Name = "inputOccurence";
-        // inputOccurence.Text = "";
-        // inputOccurence.Location = new System.Drawing.Point(300, 125);
-        // inputOccurence.Size = new System.Drawing.Size(100, 20);
+        //inputSearch
+        inputSearch.Name = "inputSearch";
+        inputSearch.Text = "";
+        inputSearch.Location = new System.Drawing.Point(300, 100);
+        inputSearch.Size = new System.Drawing.Size(100, 20);
+        //inputOccurence
+        inputOccurence.Name = "inputOccurence";
+        inputOccurence.Text = "";
+        inputOccurence.Location = new System.Drawing.Point(300, 125);
+        inputOccurence.Size = new System.Drawing.Size(100, 20);
 
-        // //button recopier
-        // btnRecopier.Name = "btnRecopier";
-        // btnRecopier.Text = "Recopier";
-        // btnRecopier.Location = new System.Drawing.Point(500, 50);
+        //button recopier
+        btnRecopier.Name = "btnRecopier";
+        btnRecopier.Text = "Recopier";
+        btnRecopier.Location = new System.Drawing.Point(500, 50);
 
-        // //button effacer
-        // btnEffacer.Name = "btnEffacer";
-        // btnEffacer.Text = "Effacer";
-        // btnEffacer.Location = new System.Drawing.Point(500, 80);
+        //button effacer
+        btnEffacer.Name = "btnEffacer";
+        btnEffacer.Text = "Effacer";
+        btnEffacer.Location = new System.Drawing.Point(500, 80);
 
-        // //button quitter
-        // btnQuitter.Name = "btnQuitter";
-        // btnQuitter.Text = "Quitter";
-        // btnQuitter.Location = new System.Drawing.Point(500, 110);
+        //button quitter
+        btnQuitter.Name = "btnQuitter";
+        btnQuitter.Text = "Quitter";
+        btnQuitter.Location = new System.Drawing.Point(500, 110);
 
-        // //btnSearch
-        // btnSearch.Name = "btnSearch";
-        // btnSearch.Text = "Lancer recherche";
-        // btnSearch.Location = new System.Drawing.Point(425, 100);
-        // btnSearch.Size = new System.Drawing.Size(130, 20);
-        // //checkbox
-        // chkModifier.Name = "chkModifier";
-        // chkModifier.Text = "Modifier la couleur";
-        // chkModifier.Location = new System.Drawing.Point(50, 120);
-        // chkModifier.Size = new System.Drawing.Size(200, 20);
-        // this.chkModifier.CheckedChanged += new EventHandler(chkModifier_CheckedChanged);
+        //btnSearch
+        btnSearch.Name = "btnSearch";
+        btnSearch.Text = "Lancer recherche";
+        btnSearch.Location = new System.Drawing.Point(425, 100);
+        btnSearch.Size = new System.Drawing.Size(130, 20);
+        //checkbox
+        chkModifier.Name = "chkModifier";
+        chkModifier.Text = "Modifier la couleur";
+        chkModifier.Location = new System.Drawing.Point(50, 120);
+        chkModifier.Size = new System.Drawing.Size(200, 20);
+        this.chkModifier.CheckedChanged += new EventHandler(chkModifier_CheckedChanged);
 
-        // //add
-        // this.Controls.Add(gbxCouleur);
-        // this.Controls.Add(gbPhrase);
-        // this.Controls.Add(gbText);
+        //add
+        this.Controls.Add(gbxCouleur);
+        this.Controls.Add(gbPhrase);
+        this.Controls.Add(gbText);
         this.Controls.Add(gbComboBox);
 
         // // this.Controls.Add(lblOriginal);
--- Winforms-calc/Form1.cs
+++ Winforms-calc/Form1.cs
@@ -2,115 +2,117 @@ namespace Winforms_calc;
 
 public partial class Form1 : Form
 {
-    // private string leTexte;
+     private string leTexte;
     public Form1()
     {
         InitializeComponent();
-        // btnRecopier.Click += btnRecopier_Click;
-        // btnEffacer.Click += btnEffacer_Click;
-        // btnQuitter.Click += btnQuitter_Click;
-        // this.input.GotFocus += new EventHandler(txtInput_GotFocus);
-        // this.leTexte = "Entrer le texte initial";
-        // this.input.Text = leTexte;
-        // this.Height = 400;
-        // btnSearch.Click += btnChercher_Click;
+        btnRecopier.Click += btnRecopier_Click;
+        btnEffacer.Click += btnEffacer_Click;
+        btnQuitter.Click += btnQuitter_Click;
+        this.input.GotFocus += new EventHandler(txtInput_GotFocus);
+        this.leTexte = "Entrer le texte initial";
+        this.input.Text = leTexte;
+        this.Height = 300;
+        btnSearch.Click += btnChercher_Click;
         this.Init();
         btnAjoute.Click += btnAdd_Click;
         btnAjouteTout.Click += btnAddAll_Click;
         btnSupprime.Click += btnRemove_Click;
         btnSupprimeTout.Click += btnRemoveAll_Click;
+        btnBas.Click += btnBas_Click;
+   
     }
 
-    // private void btnRecopier_Click(object sender, EventArgs e)
-    // {
-    //     this.lblResultat.Text = this.input.Text;
-    // }
-    // private void btnEffacer_Click(object sender, EventArgs e)
-    // {
-    //     this.input.Text = this.leTexte;
-    //     this.lblResultat.Text = "";
-    // }
-    // private void btnQuitter_Click(object sender, EventArgs e)
-    // {
-    //     if (MessageBox.Show("Voulez vous vraiment quitter", "Terminer ?", MessageBoxButtons.OKCancel) == DialogResult.OK)
-    //     {
-    //         Application.Exit();
-    //     }
-    // }
-    // private void Form1_Load(object sender, EventArgs e)
-    // {
-    //     this.input.Text = "Entrer le texte initial";
-    // }
-    // private void txtInput_GotFocus(object sender, EventArgs e)
-    // {
-    //     this.input.Text = "";
-    // }
-    // private void chkModifier_CheckedChanged(object sender, EventArgs e)
-    // {
-    //     this.gbxCouleur.Visible = this.chkModifier.Checked;
-    //     this.Height = this.chkModifier.Checked ? 800 : 400;
-    // }
-    // private void rbColor_CheckedChanged(object sender, EventArgs e)
-    // {
-    //     if (this.rbVert.Checked)
-    //     {
-    //         this.lblColor.BackColor = System.Drawing.Color.Green;
-    //     }
-    //     else if (this.rbRouge.Checked)
-    //     {
-    //         this.lblColor.BackColor = System.Drawing.Color.Red;
-    //     }
-    //     else
-    //     {
-    //         this.lblColor.BackColor = System.Drawing.Color.Blue;
-    //     }
+    private void btnRecopier_Click(object sender, EventArgs e)
+    {
+        this.lblResultat.Text = this.input.Text;
+    }
+    private void btnEffacer_Click(object sender, EventArgs e)
+    {
+        this.input.Text = this.leTexte;
+        this.lblResultat.Text = "";
+    }
+    private void btnQuitter_Click(object sender, EventArgs e)
+    {
+        if (MessageBox.Show("Voulez vous vraiment quitter", "Terminer ?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+        {
+            Application.Exit();
+        }
+    }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        this.input.Text = "Entrer le texte initial";
+    }
+    private void txtInput_GotFocus(object sender, EventArgs e)
+    {
+        this.input.Text = "";
+    }
+    private void chkModifier_CheckedChanged(object sender, EventArgs e)
+    {
+        this.gbxCouleur.Visible = this.chkModifier.Checked;
+        this.Height = this.chkModifier.Checked ? 1000 : 300;
+    }
+    private void rbColor_CheckedChanged(object sender, EventArgs e)
+    {
+        if (this.rbVert.Checked)
+        {
+            this.lblColor.BackColor = System.Drawing.Color.Green;
+        }
+        else if (this.rbRouge.Checked)
+        {
+            this.lblColor.BackColor = System.Drawing.Color.Red;
+        }
+        else
+        {
+            this.lblColor.BackColor = System.Drawing.Color.Blue;
+        }
 
-    // }
-    // private void btnChercher_Click(object sender, EventArgs e)
-    // {
-    //     Int32 nombreOccurences = 0;
-    //     if (this.inputSearch.Text.Length == 0)
-    //     {
-    //         MessageBox.Show("vous n'avez pas renseigné de caractère");
-    //         this.inputSearch.Focus();
-    //     }
-    //     else
-    //     {
-    //         this.inputSearch.Text = this.inputSearch.Text.Substring(0, 1);
-    //         if (this.rbPhrase1.Checked)
-    //         {
-    //             nombreOccurences += rechercheCaractere(this.inputPhrase1.Text, Char.Parse(this.inputSearch.Text));
-    //         }
-    //         if (this.rbPhrase2.Checked)
-    //         {
-    //             nombreOccurences += rechercheCaractere(this.inputPhrase2.Text, Char.Parse(this.inputSearch.Text));
-    //         }
-    //         if (this.rbPhrase3.Checked)
-    //         {
-    //             nombreOccurences += rechercheCaractere(this.inputPhrase3.Text, Char.Parse(this.inputSearch.Text));
-    //         }
-    //         this.inputOccurence.Text = nombreOccurences.ToString();
-    //     }
-    // }
-    // private Int32 rechercheCaractere(string zone, Char caractereRecherche)
-    // {
-    //     Int32 longZone;
-    //     Char caractereElementaire;
-    //     Int32 nbOccurences = 0;
-    //     longZone = zone.Length;
+    }
+    private void btnChercher_Click(object sender, EventArgs e)
+    {
+        Int32 nombreOccurences = 0;
+        if (this.inputSearch.Text.Length == 0)
+        {
+            MessageBox.Show("vous n'avez pas renseigné de caractère");
+            this.inputSearch.Focus();
+        }
+        else
+        {
+            this.inputSearch.Text = this.inputSearch.Text.Substring(0, 1);
+            if (this.rbPhrase1.Checked)
+            {
+                nombreOccurences += rechercheCaractere(this.inputPhrase1.Text, Char.Parse(this.inputSearch.Text));
+            }
+            if (this.rbPhrase2.Checked)
+            {
+                nombreOccurences += rechercheCaractere(this.inputPhrase2.Text, Char.Parse(this.inputSearch.Text));
+            }
+            if (this.rbPhrase3.Checked)
+            {
+                nombreOccurences += rechercheCaractere(this.inputPhrase3.Text, Char.Parse(this.inputSearch.Text));
+            }
+            this.inputOccurence.Text = nombreOccurences.ToString();
+        }
+    }
+    private Int32 rechercheCaractere(string zone, Char caractereRecherche)
+    {
+        Int32 longZone;
+        Char caractereElementaire;
+        Int32 nbOccurences = 0;
+        longZone = zone.Length;
 
-    //     for (int i = 0; i < longZone; i++)
-    //     {
-    //         caractereElementaire = zone[i];
-    //         if (caractereElementaire == caractereRecherche)
-    //         {
-    //             nbOccurences++;
-    //         }
+        for (int i = 0; i < longZone; i++)
+        {
+            caractereElementaire = zone[i];
+            if (caractereElementaire == caractereRecherche)
+            {
+                nbOccurences++;
+            }
 
-    //     }
-    //     return nbOccurences;
+        }
+        return nbOccurences;
 
-    // }
+    }
     private void Init()
     {
         this.cbxSource.Items.Clear();
@@ -158,7 +160,7 @@ public partial class Form1 : Form
             this.btnSupprime.Enabled = false;
             this.btnSupprimeTout.Enabled = false;
         }
-           if (this.cbxSource.Items.Count > 0)
+        if (this.cbxSource.Items.Count > 0)
         {
             this.btnAjoute.Enabled = true;
             this.btnAjouteTout.Enabled = true;
@@ -176,6 +178,17 @@ public partial class Form1 : Form
         this.btnAjoute.Enabled = true;
         this.btnAjouteTout.Enabled = true;
 
+    }
+    private void btnBas_Click(object sender, EventArgs e)
+    {
+        Object temp;
+        if (this.listCible.SelectedIndex != this.listCible.Items.Count - 1)
+        {
+            temp = this.listCible.SelectedItem;
+            this.listCible.Items[this.listCible.SelectedIndex] = this.listCible.Items[this.listCible.SelectedIndex + 1];
+            this.listCible.Items[this.listCible.SelectedIndex + 1] = temp;
+            this.listCible.SelectedIndex++;
+        }
     }
 
 }
