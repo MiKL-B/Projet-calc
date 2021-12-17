0a522e3a9d1a4b2a8da871a7804c3f408a94401e
michael <michaelbecquer@yahoo.fr>
2021-12-17 11:54:49 +0100

    yannick mode

--- Winforms-calc/Form1.Designer.cs
+++ Winforms-calc/Form1.Designer.cs
@@ -29,7 +29,8 @@ partial class Form1
     }
     private GroupBox gbAffichage = new GroupBox(), gbNumber = new GroupBox();
     private ComboBox comboVariable = new ComboBox(), comboFunction = new ComboBox();
-    private int xPos = 50, yPos = 10;
+    private int xPos = 0, yPos = 10;
+    private TextBox textAffichage = new TextBox();
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
@@ -37,7 +38,7 @@ partial class Form1
         this.ClientSize = new System.Drawing.Size(400, 650);
         this.Text = "Form1";
 
-        string[] mesButtons = new string[31] { "(", ")", ";", "Lisp", "RPN", "Memory", "Affect", "Unary", "CE", "C", "Return", "1/x", "x²", "²√x", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ",", "=" };
+        string[] mesButtons = new string[32] { "Y Mode","Lisp", "RPN", "Memory", "Affect", "Unary", "CE", "C", "Return", ".", "(", ")", ";", "1/x", "²", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ",", "=" };
 
         for (int i = 0; i < mesButtons.Length; i++)
         {
@@ -47,23 +48,34 @@ partial class Form1
                 this.xPos = 50;
                 this.yPos = this.yPos + 50;
             }
-            gbNumber.Controls.Add(new ButtonCalc(mesButtons[i], this.xPos, this.yPos));
+            ButtonCalc item = new ButtonCalc(mesButtons[i], this.xPos, this.yPos);
+            item.Click += button_Click;
+            gbNumber.Controls.Add(item);
         }
 
         gbNumber.Location = new System.Drawing.Point(0, 220);
         gbNumber.Size = new System.Drawing.Size(400, 450);
         gbNumber.Controls.AddRange(new Control[] { comboVariable, comboFunction });
 
+        gbAffichage.Controls.AddRange(new Control[] { textAffichage });
         gbAffichage.Text = "Affichage";
         gbAffichage.Location = new System.Drawing.Point(0, 20);
         gbAffichage.Size = new System.Drawing.Size(400, 200);
         gbAffichage.BackColor = Color.FromArgb(32, 32, 32);
         gbAffichage.ForeColor = Color.LightGray;
 
+        textAffichage.Location = new System.Drawing.Point(0, 20);
+        textAffichage.Size = new System.Drawing.Size(400, 0);
+        textAffichage.Font = new Font("Microsoft sans serif", 20);
+        textAffichage.BackColor = Color.FromArgb(32,32,32);
+        textAffichage.ForeColor = Color.LightGray;
+
+
         comboVariable.Text = "Variables";
         comboVariable.Location = new System.Drawing.Point(250, 0);
         comboFunction.Text = "Fonctions";
         comboFunction.Location = new System.Drawing.Point(250, 40);
+
 
         this.Controls.AddRange(new Control[] { gbAffichage, gbNumber });
     }
--- Winforms-calc/Form1.cs
+++ Winforms-calc/Form1.cs
@@ -7,6 +7,7 @@ public partial class Form1 : Form
     {
         InitializeComponent();
         this.Init();
+        
     }
 
     private void Init()
@@ -19,10 +20,33 @@ public partial class Form1 : Form
             this.comboVariable.Items.Add(c);
         }
 
-        string[] functions = new string[11]{"sqrt","pow","sin","cos","tan","asin","acos","atan","pi","e","ln"};
+        string[] functions = new string[11] { "sqrt", "pow", "sin", "cos", "tan", "asin", "acos", "atan", "pi", "e", "ln" };
         foreach (var f in functions)
         {
-           this.comboFunction.Items.Add(f);
+            this.comboFunction.Items.Add(f);
         }
     }
+
+    private void button_Click(object sender, EventArgs e)
+    {
+        ButtonCalc b = sender as ButtonCalc;
+
+        switch (b.Text)
+        {
+            case "Return":
+                textAffichage.Text = textAffichage.Text.Remove(textAffichage.Text.Length - 1);
+                break;
+            case "Y Mode":
+           b.Click += ymode_Click;
+            break;
+            default:
+                textAffichage.Text += b.Text;
+                break;
+        }
+    }
+    private void ymode_Click(object sender, EventArgs e)
+    {
+        textAffichage.Font = new Font("Microsoft sans serif", 100);
+    }
+
 }
